961ad4150e4f5b42e430bcf845b35e4a50d9cefb
Katherina Dayaon <katherinadayaon@gmail.com>
2025-12-11 11:05:15 -0500

    successful test 3D space

--- smartlook/src/App.jsx
+++ smartlook/src/App.jsx
@@ -1,73 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
+
 import * as THREE from 'three';
-import './App.css'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import Stats from 'three/examples/jsm/libs/stats.module';
+
+import SceneInit from './lib/SceneInit';
 
 function App() {
-
   useEffect(() => {
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
 
-    // Get canvas
-    const canvas = document.getElementById('myThreeJsCanvas');
-    if (!canvas) return;
-
-    // Scene
-    const scene = new THREE.Scene();
-
-    // Camera
-    const camera = new THREE.PerspectiveCamera(
-      50,
-      window.innerWidth / window.innerHeight,
-      1,
-      1000
-    );
-    camera.position.z = 96;
-
-    // Renderer
-    const renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true,
-    });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // Lighting
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(ambientLight);
-
-    const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.position.set(0, 64, 32);
-    scene.add(spotLight);
-
-    // Test Cube
     const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
     const boxMaterial = new THREE.MeshNormalMaterial();
     const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-    scene.add(boxMesh);
 
-    // orbit controls
-    const controls = new OrbitControls(camera, renderer.domElement);
-    
-
-    // Animate
-    const animate = () => {
-      // boxMesh.rotation.x += 0.01;
-      // boxMesh.rotation.y += 0.01;
-      controls.update();
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    };
-
-    animate();
+    test.scene.add(boxMesh);
   }, []);
 
   return (
-    <>
-      <canvas
-        id="myThreeJsCanvas"
-      />
-    </>
-  )
+    <div>
+      <canvas id="myThreeJsCanvas" />
+    </div>
+  );
 }
 
-export default App
+export default App;
